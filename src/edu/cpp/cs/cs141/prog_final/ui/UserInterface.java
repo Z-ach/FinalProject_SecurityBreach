31834b99d53076f84bdef2d9d5ff0cb041a3d385
z-ach <zachkaufman97@gmail.com>
2017-02-06 15:15:40 -0800

    userinterface

--- src/edu/cpp/cs/cs141/prog_final/ui/UserInterface.java
+++ src/edu/cpp/cs/cs141/prog_final/ui/UserInterface.java
@@ -1,9 +1,56 @@
 package edu.cpp.cs.cs141.prog_final.ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
+	private Scanner input;
+	
 	public UserInterface() {
-		// TODO Auto-generated constructor stub
+		
 	}
-
+	
+	public void instruction(){
+		
+	}
+	
+	public void options(){
+		
+	}
+	
+	public void takeInput(){
+		
+	}
+	
+	public void printGrid(){
+		
+	}
+	
+	public void displayInfo(){
+		
+	}
+	
+	public void saveGame(){
+		
+	}
+	
+	public void debug(){
+		
+	}
+	
+	public void errorCheck(){
+		
+	}
+	
+	public void endMessage(boolean win){
+		
+	}
+	
+	public void loseLife(){
+		
+	}
+	
+	public void briefCase(boolean hasCase){
+		
+	}
 }
