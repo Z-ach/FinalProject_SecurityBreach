530075355ef23592c00b2c365f92e6812c4ea42e
z-ach <zachkaufman97@gmail.com>
2017-02-06 15:37:48 -0800

    grid

--- src/edu/cpp/cs/cs141/prog_final/grid/Grid.java
+++ src/edu/cpp/cs/cs141/prog_final/grid/Grid.java
@@ -9,10 +9,23 @@ package edu.cpp.cs.cs141.prog_final.grid;
  */
 public class Grid {
 	
+	private int[][] board;
 	
 	
 	public Grid() {
 		
+	}
+	
+	private void generateBoard(){
+		
+	}
+	
+	private void assign(){
+		
+	}
+	
+	public int[][] getBoard(){
+		return board;
 	}
 
 }
