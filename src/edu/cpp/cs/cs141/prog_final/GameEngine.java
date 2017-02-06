5cc720321263bbdbf1c24c6b6564ab3d867a39fb
z-ach <zachkaufman97@gmail.com>
2017-02-06 15:18:19 -0800

    game engine

--- src/edu/cpp/cs/cs141/prog_final/GameEngine.java
+++ src/edu/cpp/cs/cs141/prog_final/GameEngine.java
@@ -1,6 +1,7 @@
 package edu.cpp.cs.cs141.prog_final;
 
 import edu.cpp.cs.cs141.prog_final.beings.Ninja;
+import edu.cpp.cs.cs141.prog_final.beings.Player;
 import edu.cpp.cs.cs141.prog_final.grid.Grid;
 import edu.cpp.cs.cs141.prog_final.ui.UserInterface;
 
@@ -9,6 +10,8 @@ public class GameEngine {
 	private Grid grid;
 	private Ninja[] ninjas;
 	private UserInterface ui;
+	private Player player;
+	
 	
 	public GameEngine() {
 		
