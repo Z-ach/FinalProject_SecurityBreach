8e7fa1d66e5ce4f1a14540f5bd51b168c7de6247
J <J@10.110.219.9>
2017-02-06 15:22:52 -0800

    added fields to player class

--- src/edu/cpp/cs/cs141/prog_final/beings/Player.java
+++ src/edu/cpp/cs/cs141/prog_final/beings/Player.java
@@ -1,6 +1,12 @@
 package edu.cpp.cs.cs141.prog_final.beings;
 
 public class Player {
+	
+	
+	public static int Lives = 3;
+	
+	public static int Bullet = 1;
+	
 
 	public Player() {
 		// TODO Auto-generated constructor stub
