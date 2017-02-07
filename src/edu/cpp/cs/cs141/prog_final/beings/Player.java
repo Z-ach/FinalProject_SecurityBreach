601225ab29daec529fc57ebe4a92641a723a52d3
blayala <blayala@b01-aruba-authenticated-10-110-240-217.wlan.cpp.edu>
2017-02-07 09:42:03 -0800

    I did the skeleton methods for the player class. You're welcome

--- src/edu/cpp/cs/cs141/prog_final/beings/Player.java
+++ src/edu/cpp/cs/cs141/prog_final/beings/Player.java
@@ -7,9 +7,36 @@ public class Player {
 	
 	public static int Bullet = 1;
 	
+	
 
 	public Player() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void shoot(){
+		
+	}
 
+	public void look(){
+		
+	}
+	
+	public void walk(){
+		
+	}
+	
+	public int getLives(){
+		
+		return Lives;
+	}
+	
+	public boolean alive(){
+		if(Lives > 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
 }
