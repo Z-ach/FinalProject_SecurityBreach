ea9fa95f5d2bb8f46c04728b1f6f467ecaa1d10c
z-ach <zachkaufman97@gmail.com>
2017-02-07 14:57:33 -0800

    started savegame stuff

---
+++ src/edu/cpp/cs/cs141/prog_final/io/LoadGame.java
@@ -0,0 +1,9 @@
+package edu.cpp.cs.cs141.prog_final.io;
+
+public class LoadGame {
+
+	public LoadGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/edu/cpp/cs/cs141/prog_final/io/SaveGame.java
@@ -0,0 +1,21 @@
+package edu.cpp.cs.cs141.prog_final.io;
+
+public class SaveGame {
+	
+	private int[][] boardState;
+	
+	public SaveGame() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private int[][] parseState(){
+		
+		
+		return boardState;
+	}
+	
+	public void writeToFile(){
+		
+	}
+	
+}
