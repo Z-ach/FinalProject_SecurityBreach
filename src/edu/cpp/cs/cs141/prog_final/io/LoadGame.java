d6336473c65e876238a2088c75b4ba56df89ee99
z-ach <zachkaufman97@gmail.com>
2017-02-07 19:22:45 -0800

    added savegame and loadgame classes with methods/fields and documentation

--- src/edu/cpp/cs/cs141/prog_final/io/LoadGame.java
+++ src/edu/cpp/cs/cs141/prog_final/io/LoadGame.java
@@ -1,9 +1,94 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving Professor: Edwin
+ * Rodríguez
+ *
+ * Final Group Project (Security Breach)
+ *
+ * Description of assignment: Our group is making a text-based game called
+ * Security Breach. The player starts out with at the bottom left of a 9x9
+ * grid room. 9 out of 81 rooms will contain a briefcase with classified
+ * information that the player needs to retrieve. The 9 rooms with the
+ * briefcase will be evenly distributed throughout the 9x9 grid room.
+ * However, all the rooms in the building are dark. The player is equipped
+ * with night-vision goggles which allows him or her to see two squares
+ * ahead of him. It will not be easy for the player to advance through the
+ * map because there are 6 ninja-assassins patrolling the building. If a
+ * ninja-assassin ends up in the same room as the player, the player will be
+ * stabbed loses a life. The player start out with 3 lives. Everytime a life
+ * is lost, the player is sent back to the beginning where he or she first
+ * started off. The player can protect himself by shooting his gun in any
+ * direction. If the bullet hits a ninja-assassin(s), it will die and be
+ * eliminated from game. To help the player, there are three power-up items
+ * that are randomly placed throughout the grid. This includes a
+ * invicibility shield (protects player from stabbing for 5 turns), radar
+ * (display location of the briefcase), and bullet drop (player can only
+ * have one bullet on him. If this item is picked up with the character
+ * still having a bullet, the effect will be negated).
+ * 
+ * Team: We showed up
+ * 
+ * Team members: Bryan Ayala, Annalyn Edulag, Kelvin Huang, Zachary Kaufman,
+ * Michael Jason Yan
+ * 
+ */
 package edu.cpp.cs.cs141.prog_final.io;
 
+import java.util.Scanner;
+
+/**
+ * This class will be responsible for restoring a previously saved game state.
+ * The class contains methods that will read an old game state from a file, then
+ * parse that information, and generate a board and game state that is identical
+ * to the one that is saved in the file.
+ */
 public class LoadGame {
 
-	public LoadGame() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * This field represents the two dimensional array of integers that
+	 * represent the old state of the game that was stored in the file. The int
+	 * values within this array correspond to different objects within the game.
+	 */
+	private int[][] boardState;
+
+	/**
+	 * This field represents a {@link java.util.Scanner} object that will read
+	 * the file line by line.
+	 */
+	private Scanner reader;
+
+	/**
+	 * This method will initialize the necessary fields that will be required to
+	 * restore the game to the previous state that was stored in the file.
+	 * 
+	 * @param fileName
+	 *            the name of the file the game will be loaded from
+	 */
+	public LoadGame(String fileName) {
+
+	}
+
+	/**
+	 * This method will read in the state of the game from the file and parse it
+	 * into the two dimensional array represented by {@link #boardState}.
+	 * 
+	 * @return the old board state that was saved in the file, now as a two
+	 *         dimensional array
+	 */
+	public int[][] readBoardState() {
+		return boardState;
+	}
+
+	/**
+	 * This method takes in the two dimensional array holding the old state of
+	 * the board and assigns the values in the array that represent various
+	 * objects to actual objects in the game.
+	 * 
+	 * @param saveState
+	 *            the old state of the game that will be used to assign objects
+	 *            to the new game board
+	 */
+	public void restoreActualGrid(int[][] saveState) {
+
 	}
 
 }
--- src/edu/cpp/cs/cs141/prog_final/io/SaveGame.java
+++ src/edu/cpp/cs/cs141/prog_final/io/SaveGame.java
@@ -1,21 +1,97 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving Professor: Edwin
+ * Rodríguez
+ *
+ * Final Group Project (Security Breach)
+ *
+ * Description of assignment: Our group is making a text-based game called
+ * Security Breach. The player starts out with at the bottom left of a 9x9
+ * grid room. 9 out of 81 rooms will contain a briefcase with classified
+ * information that the player needs to retrieve. The 9 rooms with the
+ * briefcase will be evenly distributed throughout the 9x9 grid room.
+ * However, all the rooms in the building are dark. The player is equipped
+ * with night-vision goggles which allows him or her to see two squares
+ * ahead of him. It will not be easy for the player to advance through the
+ * map because there are 6 ninja-assassins patrolling the building. If a
+ * ninja-assassin ends up in the same room as the player, the player will be
+ * stabbed loses a life. The player start out with 3 lives. Everytime a life
+ * is lost, the player is sent back to the beginning where he or she first
+ * started off. The player can protect himself by shooting his gun in any
+ * direction. If the bullet hits a ninja-assassin(s), it will die and be
+ * eliminated from game. To help the player, there are three power-up items
+ * that are randomly placed throughout the grid. This includes a
+ * invicibility shield (protects player from stabbing for 5 turns), radar
+ * (display location of the briefcase), and bullet drop (player can only
+ * have one bullet on him. If this item is picked up with the character
+ * still having a bullet, the effect will be negated).
+ * 
+ * Team: We showed up
+ * 
+ * Team members: Bryan Ayala, Annalyn Edulag, Kelvin Huang, Zachary Kaufman,
+ * Michael Jason Yan
+ * 
+ */
 package edu.cpp.cs.cs141.prog_final.io;
 
+import java.io.PrintWriter;
+
+import edu.cpp.cs.cs141.prog_final.grid.Grid;
+
+/**
+ * This class is designed to save the state of the game to a file. The state of
+ * the game will first be parsed so that it can be recorded to a file. The text
+ * will then be written to a file in a specific manner so that the file can be
+ * loaded and played at a later date.
+ */
 public class SaveGame {
-	
+
+	/**
+	 * This field represents the what the current state of the game will be
+	 * parsed into. The {@link edu.cpp.cs.cs141.prog_final.grid.Grid} holds the
+	 * state of the game, so it will be parsed into this field.
+	 */
 	private int[][] boardState;
-	
-	public SaveGame() {
-		// TODO Auto-generated constructor stub
+
+	/**
+	 * This field represents an object of type {@link java.io.PrintWriter} This
+	 * is used to print the state of the game into a file.
+	 */
+	private PrintWriter writer;
+
+	/**
+	 * Constructor for {@link SaveGame}. This will make the necessary
+	 * initializations that will be required to save the state of the game.
+	 * 
+	 * @param fileName the name of the file that the game will be saved to
+	 */
+	public SaveGame(String fileName) {
+
 	}
-	
-	private int[][] parseState(){
-		
-		
+
+	/**
+	 * This method will take in the game's grid and parse it into a two
+	 * dimensional array with ints that represents specific objects within the
+	 * game.
+	 * 
+	 * @param grid
+	 *            the game's grid
+	 * @return a two dimensional array with the current state of the game
+	 */
+	private int[][] parseState(Grid grid) {
+
 		return boardState;
 	}
-	
-	public void writeToFile(){
-		
+
+	/**
+	 * This method will do the physical writing to the file. The grid will be
+	 * passed in as an argument, which will be used in the method call to
+	 * {@link #parseState(Grid)}. The method will then print all data into a
+	 * file.
+	 * 
+	 * @param grid the game's grid
+	 */
+	public void writeToFile(Grid grid) {
+
 	}
-	
+
 }
