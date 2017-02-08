b1615a9a4be17f0f2bc5e0c019041001ebf07805
z-ach <zachkaufman97@gmail.com>
2017-02-07 21:25:39 -0800

    minor changes to javadocs

--- src/edu/cpp/cs/cs141/prog_final/ui/UserInterface.java
+++ src/edu/cpp/cs/cs141/prog_final/ui/UserInterface.java
@@ -27,149 +27,142 @@
  */
 package edu.cpp.cs.cs141.prog_final.ui;
 
-/**
-* The UserInterface class represents the game interactions with the player
-* throughout the game. The user is greeted with instructions, the grid
-* is printed for the game to begin which then shows the options for the player
-*  to move, shoot, or debug, save or exit the game, status of powerups and 
-*  bullets, and the end result of the game is displayed. 
-*/
-
-
 import java.util.Scanner;
 
+/**
+ * The UserInterface class represents the game interactions with the player
+ * throughout the game. The user is greeted with instructions, the grid is
+ * printed for the game to begin which then shows the options for the player to
+ * move, shoot, or debug, save or exit the game, status of powerups and bullets,
+ * and the end result of the game is displayed.
+ */
 public class UserInterface {
 
 	/**
-	 * This field represents the {@link java.util.Scanner} that creates
-	 * the scanner that will take the user input to play the game and
-	 * to print each of the objects within the game to the user.
+	 * This field represents the {@link java.util.Scanner} object that creates
+	 * the scanner that will take the user input to play the game and to print
+	 * each of the objects within the game to the user.
 	 */
 	private Scanner input;
-	
+
 	/**
 	 * The default constructor for the class {@link UserInterface}. This
-	 * constructor will allow for the {@link edu.cpp.cs.cs141.prog_final.GameEngine}
-	 * to display the game outputs to the user.
-	 *  
+	 * constructor will allow for the
+	 * {@link edu.cpp.cs.cs141.prog_final.GameEngine} to display the game
+	 * outputs to the user. Necessary things will be initialized here, such as
+	 * {@link #input}
+	 * 
 	 */
 	public UserInterface() {
-		
+
 	}
-	
+
 	/**
-	 * This method represents the {@link instruction} of how to play
-	 * the game to the user. The field will explain to the user what
-	 * keys to use to play the game, what the powerups do, and the
-	 * end objective to find the hidden briefcase through describing 
-	 * the setting. 
+	 * This method represents the {@link #instruction()} of how to play the game to
+	 * the user. The field will explain to the user what keys to use to play the
+	 * game, what the powerups do, and the end objective to find the hidden
+	 * briefcase through describing the setting.
 	 */
-	public void instruction(){
-		
+	public void instruction() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@link playerOptions} prints out the 
-	 * player's options of looking, shooting, and debugging. These
-	 * options will be displayed 
+	 * This method represents the {@link #playerOptions()} prints out the player's
+	 * options of looking, shooting, and debugging. These options will be
+	 * displayed
 	 */
-	public void playerOptions(){
-		
+	public void playerOptions() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@Link exitOptions} that will
-	 * allow for the player to either save the game or exit. These
-	 * options will be available to the player throughout the game. 
+	 * This method represents the {@link #exitOptions()} that will allow for the
+	 * player to either save the game or exit. These options will be available
+	 * to the player throughout the game.
 	 */
-	public void exitOptions(){
-		
+	public void exitOptions() {
+
 	}
-	
+
 	/**
-	 * This method will {@Link takeInput} from the player through
-	 * asking the player to enter their desired options of looking,
-	 * shooting, debugging, saving the game, loading a game, or
-	 * exiting.
+	 * This method will {@link takeInput()} from the player through asking the
+	 * player to enter their desired options of looking, shooting, debugging,
+	 * saving the game, loading a game, or exiting.
 	 */
-	public void takeInput(){
-		
+	public void takeInput() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@link printGrid} which serves
-	 * as the base view of the game that will hold each of the objects.
-	 * The player must navigate through the grid in order to play.
+	 * This method represents the {@link #printGrid()} which serves as the base
+	 * view of the game that will hold each of the objects. The player must
+	 * navigate through the grid in order to play.
 	 */
-	public void printGrid(){
-		
+	public void printGrid() {
+
 	}
-	
+
 	/**
-	 * This method {@link displayInfo} represents the status of
-	 * lives that the user has and notifies the user if any
-	 * power ups are being used during the player's turn.
+	 * This method {@link #displayInfo()} represents the status of lives that the
+	 * user has and notifies the user if any power ups are being used during the
+	 * player's turn.
 	 */
-	public void displayInfo(){
-		
+	public void displayInfo() {
+
 	}
-	
+
 	/**
-	 * The method of {@link saveGame} represents what the system
-	 * will ask after they choose the option to save the game. The
-	 * input for a file name will be asked from the player in order
-	 * to successfully save it.
+	 * The method of {@link #saveGame()} represents what the system will ask after
+	 * they choose the option to save the game. The input for a file name will
+	 * be asked from the player in order to successfully save it.
 	 */
-	public void saveGame(){
-		
+	public void saveGame() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@link debug} option that
-	 * allows for the all components of the grid to be
-	 * visible. Debug mode option will be available for the
-	 * player throughout the game.
+	 * This method represents the {@link #debug()} option that allows for the all
+	 * components of the grid to be visible. Debug mode option will be available
+	 * for the player throughout the game.
 	 */
-	public void debug(){
-		
+	public void debug() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@link errorCheck} to display
-	 * an error message when the user attempts to move in 
-	 * directions that are outside the grid space. 
+	 * This method represents the {@link #errorCheck()} to display an error message
+	 * when the user attempts to move in directions that are outside the grid
+	 * space.
 	 */
-	public void errorCheck(){
-		
+	public void errorCheck() {
+
 	}
-	
+
 	/**
-	 * This method is {@ code true} if the character has found 
-	 * the briefcase and the message telling the character has
-	 * won. If {@code false}, the system tells the user that
-	 * the game is over.
+	 * This method is {@code true} if the character has found the briefcase and
+	 * the message telling the character has won. If {@code false}, the system
+	 * tells the user that the game is over.
 	 */
-	public void endMessage(boolean win){
-		
+	public void endMessage(boolean win) {
+
 	}
-	
+
 	/**
-	 * The method represents when the character will {@link loselife}
-	 * when they are stabbed by a ninja. The system will tell the
-	 * character that they have lost a life and have respawned to 
-	 * their initial placement on the grid.  
+	 * The method represents when the character will {@link #loseLife()} when they
+	 * are stabbed by a ninja. The system will tell the character that they have
+	 * lost a life and have respawned to their initial placement on the grid.
 	 */
-	public void loseLife(){
-		
+	public void loseLife() {
+
 	}
-	
+
 	/**
-	 * This method represents the {@link briefcase} that 
-	 * needs to be found by the player in order to win the
-	 * game. If {@code true} then the message tells the
-	 * player that they have successfully found the briefcase.
+	 * This method represents the {@link #briefCase(boolean)} that needs to be found by
+	 * the player in order to win the game. If {@code true} then the message
+	 * tells the player that they have successfully found the briefcase.
 	 */
-	public void briefCase(boolean hasCase){
-		
+	public void briefCase(boolean hasCase) {
+
 	}
 }
