ddbea2b20cf2ec7451c0a9b21f79fc5ebaed052b
z-ach <zachkaufman97@gmail.com>
2017-02-07 22:31:25 -0800

    changes to javadocs, position is now only 1d array

--- src/edu/cpp/cs/cs141/prog_final/beings/LivingBeing.java
+++ src/edu/cpp/cs/cs141/prog_final/beings/LivingBeing.java
@@ -1,39 +1,47 @@
 package edu.cpp.cs.cs141.prog_final.beings;
 
 /**
- * This is the LivingBeing class. This class is a super class 
- * and an abstract class. It passes on attributes to the player
- * and ninja subclasses. No instances can be made from this class,
- * therefore the subclasses inherit this classes methods.
+ * This is the LivingBeing class. This class is a super class and an abstract
+ * class. It passes on attributes to the {@link Player} and {@link Ninja}
+ * subclasses. No instances can be made from this class, therefore the
+ * subclasses inherit this classes methods.
  */
 public abstract class LivingBeing {
 
-	private int[][] position;
 	/**
-	 * This is the constructor for the LivingBeing astract class. Its parameter is
-	 * is a postion on the grid. When the constructor is called, it will
-	 * take in the parameter of the postion and created the living being on that
-	 * position on the grid. The constructor essentially spawns the lving being 
-	 * when it is called on.
+	 * This field represents an array of integers that represent the spy's
+	 * location in the building. The 0th index in the array represents the row,
+	 * and the 1st index in the array represents the column. This will be
+	 * initially set by the constructors of the subclasses, and then will be
+	 * managed in this class.
 	 */
-	public LivingBeing(int[][] position) {
-		
+	private int[] position;
+
+	/**
+	 * This is the constructor for the {@link LivingBeing} abstract class. It's
+	 * parameter is a position on the grid. When the constructor is called, it
+	 * will take in the parameter of the position and created the living being
+	 * on that position on the grid. The constructor essentially spawns the
+	 * living being when it is called on.
+	 */
+	public LivingBeing(int[] position) {
+
 	}
-	
+
 	/**
-	 * This is the getPostion method. Its job is to return the position of
-	 * the living being back to the User Interface class so that it can
-	 * display the information to the user
+	 * This is the getPostion method. Its job is to return the position of the
+	 * living being back to the User Interface class so that it can display the
+	 * information to the user
 	 */
-	public int[][] getPosition(){
+	public int[] getPosition() {
 		return position;
 	}
-	
+
 	/**
-	 * This is the move class. It recieves a pramater, direction, and passes 
-	 * it on to the subclasses and tells them when to move.`
+	 * This is the move class. It receives a parameter, direction, and passes it
+	 * on to the subclasses and tells them when to move.
 	 */
-	public void move(int direction){
-		
+	public void move(int direction) {
+
 	}
 }
--- src/edu/cpp/cs/cs141/prog_final/beings/Player.java
+++ src/edu/cpp/cs/cs141/prog_final/beings/Player.java
@@ -1,85 +1,96 @@
 package edu.cpp.cs.cs141.prog_final.beings;
 
-
 /**
- * This is the player subclass. It is charge of all the attributes that a 
- * player would need. It has methods that deal with different attributes
- * and a constructor that spawns and creates a player based on a location 
- * on the grid. It inherites properties from the living being super class.
+ * This is the player subclass. It is charge of all the attributes that a player
+ * would need. It has methods that deal with different attributes and a
+ * constructor that spawns and creates a player based on a location on the grid.
+ * It inherits properties from the living being super class.
  */
-public class Player extends LivingBeing{
-	
+public class Player extends LivingBeing {
+
 	/**
-	 * This field represents the amount of lives that the player has. 
-	 * The player starts off with three lives.
+	 * This field represents the amount of lives that the player has. The player
+	 * starts off with three lives, so this value is initially {@code 3}. If the
+	 * player loses all 3 lives, the game is over, and the player has lost the
+	 * game.
 	 */
 	private static int lives = 3;
+
 	/**
-	 * This field represents the amount of bullets that the player has
-	 * in their gun. The max amount of bullets a gun can carry is one.
+	 * This field represents the amount of bullets that the player has in their
+	 * gun. The max amount of bullets a gun can carry is one. Initially
+	 * {@code 1}, will decrease by {@code 1} whenever a shot is taken. If the
+	 * player has no bullets, then no shot can be taken.
 	 */
-	private static int bullet = 1;	
-	
-	
+	private static int bullet = 1;
+
 	/**
-	 * This is the constructor for the player subclass. Its parameter is
-	 * is a postion on the grid. When the constructor is called, it will
-	 * take in the parameter of the postion and created the player on that
-	 * position on the grid. The constructor essentially spawns the player 
-	 * when it is called on.
+	 * This is the constructor for the player subclass. Its parameter is is a
+	 * position on the grid. When the constructor is called, it will take in the
+	 * parameter of the position and created the player on that position on the
+	 * grid. The constructor essentially spawns the player when it is called on.
+	 * 
+	 * @param position
+	 *            the location of where to spawn the player
 	 */
-	public Player(int[][] position) {
+	public Player(int[] position) {
 		super(position);
 	}
+
 	/**
-	 * This is the shoot method. Whenever this method is called the player
-	 * where shoot a bullet in whichever direction the player chooses.
-	 * Once the bullet makes contact with an enemy, the enemy will die.
+	 * This is the shoot method. Whenever this method is called the player will
+	 * shoot a bullet in whichever direction the player chooses. Once the bullet
+	 * makes contact with an enemy, the enemy will die. Will decrease the
+	 * {@link #bullet} amount by {@code 1}.
 	 */
-	public void shoot(){
-		
+	public void shoot() {
+
 	}
-/**
- * This is look method. Whenever this method is called the plaeyr will look in 
- * whichever direction the player chooses. Once the player chooses where to look,
- * the next two steps will be lit.
- */
-	public void look(){
-		
-	}
+
 	/**
-	 * This is the getBullets method. It returns the number of bullets left
-	 * in the players gun back to the User Interface class and displays it 
-	 * for the player
+	 * This is look method. Whenever this method is called the player will look
+	 * in whichever direction the player chooses. Once the player chooses where
+	 * to look, the next two steps will be lit.
 	 */
-	public int getBullets(){
-		
+	public void look() {
+
+	}
+
+	/**
+	 * This is the getBullets method. It returns the number of bullets left in
+	 * the players gun back to the User Interface class and displays it for the
+	 * player. Also used to determine whether or not the player can take a shot,
+	 * if there are no bullets a shot cannot be taken.
+	 */
+	public int getBullets() {
+
 		return bullet;
 	}
-	
+
 	/**
-	 * This is the getLives method. It returns the number of lives left
-	 * that the player has back to the User Interface class and displays it 
-	 * for the player
+	 * This is the getLives method. It returns the number of lives left that the
+	 * player has back to the User Interface class and displays it for the
+	 * player. Also used in {@link edu.cpp.cs.cs141.prog_final.GameEngine}.
 	 */
-	public int getLives(){
-		
+	public int getLives() {
+
 		return lives;
 	}
-	
+
 	/**
-	 * This is the alive class. Its main purpose is to check if the player
-	 * has over 0 lives left. If the player has over 0 lives, the method will
-	 * return true meaning the player is alive. If the player is dead, the method
-	 * will return false
+	 * This is the alive class. Its main purpose is to check if the player has
+	 * over {@code 0} lives left. If there are no lives left, the game is over and the
+	 * player has lost.
+	 * 
+	 * @return {@code true} if the player is alive. If the player is dead, the
+	 *         method will return {@code false}
 	 */
-	public boolean alive(){
-		if(lives > 0){
+	public boolean alive() {
+		if (lives > 0) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
+
 }
