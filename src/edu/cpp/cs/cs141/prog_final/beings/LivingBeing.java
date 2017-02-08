b8b0cc265b8f2af800a6f7ee2bc6752bda40a14c
z-ach <zachkaufman97@gmail.com>
2017-02-07 22:38:20 -0800

    fixed @param and @return things for javadocs

--- src/edu/cpp/cs/cs141/prog_final/beings/LivingBeing.java
+++ src/edu/cpp/cs/cs141/prog_final/beings/LivingBeing.java
@@ -23,6 +23,8 @@ public abstract class LivingBeing {
 	 * will take in the parameter of the position and created the living being
 	 * on that position on the grid. The constructor essentially spawns the
 	 * living being when it is called on.
+	 * 
+	 * @param position the position on the grid to create the {@link LivingBeing}
 	 */
 	public LivingBeing(int[] position) {
 
@@ -32,6 +34,8 @@ public abstract class LivingBeing {
 	 * This is the getPostion method. Its job is to return the position of the
 	 * living being back to the User Interface class so that it can display the
 	 * information to the user
+	 * 
+	 * @return the position of the {@link LivingBeing}
 	 */
 	public int[] getPosition() {
 		return position;
@@ -40,6 +44,8 @@ public abstract class LivingBeing {
 	/**
 	 * This is the move class. It receives a parameter, direction, and passes it
 	 * on to the subclasses and tells them when to move.
+	 * 
+	 * @param direction the direction to move the {@link LivingBeing}
 	 */
 	public void move(int direction) {
 
