4469853ab07c36b52f9f1f30fd4b6041cdab857e
z-ach <zachkaufman97@gmail.com>
2017-02-22 21:43:37 -0800

    started changes

---
+++ src/edu/cpp/cs/cs141/prog_final/Room.java
@@ -0,0 +1,20 @@
+package edu.cpp.cs.cs141.prog_final;
+
+public class Room {
+
+	private int x, y;
+	
+	public Room(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+}
